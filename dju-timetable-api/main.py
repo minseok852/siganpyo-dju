89bf914b520fecb082f89eeeacc086d50aec6cf4
박민석 <minseock852@naver.com>
2026-01-26 21:27:45 +0900

    CORS 연결을 위해 수정

--- dju-timetable-api/main.py
+++ dju-timetable-api/main.py
@@ -23,7 +23,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",  # Vite 개발서버
         "http://localhost:3000",
-        "https://dju-timetable.vercel.app",  # 배포 도메인 (나중에 수정)
+        "https://siganpyo-dju.vercel.app" #배포된 도메인
     ],
     allow_credentials=True,
     allow_methods=["*"],
